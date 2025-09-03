47e3d2638cf96085b27bcd5f2c3b692b8c7bbd10
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-09-02 20:49:32 -0400

    chore: add basic router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
-import MovieCard from "./components/MovieCard.jsx";
 import Home from "./pages/Home.jsx";
+import Favorites from "./pages/Favorites.jsx";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </main>
   );
 }
 
