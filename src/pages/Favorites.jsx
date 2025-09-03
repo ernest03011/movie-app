dadd77f8466fd1a69f50fd1aa87b2e98be5a12c7
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-09-03 11:22:18 -0400

    chore: load the movies marked as favorites

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,7 +1,20 @@
 import "../css/Favorites.css";
+import useMovieContext from "../contexts/useMovieContext";
+import MovieCard from "../components/MovieCard";
 
 function Favorite() {
-  return (
+  const { favorites } = useMovieContext();
+
+  return favorites ? (
+    <div className="favorites">
+      <h2>Your Favorites</h2>
+      <div className="movies-grid">
+        {favorites.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  ) : (
     <div className="favorites-empty">
       <h2>No Favorite Movies Yet</h2>
       <p>Start adding movies to your favorites and they will appear here</p>
