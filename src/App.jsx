f82c38a9b844a744cbb6e6f928cfa489e4465afa
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-09-02 21:00:33 -0400

    chore: add a navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import "./App.css";
 import Home from "./pages/Home.jsx";
 import Favorites from "./pages/Favorites.jsx";
 import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar.jsx";
 
 function App() {
   return (
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Routes>
-    </main>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
